dcc99eecd76ca2e6285447990aca873f4695f1cc
jw <orochi13@naver.com>
2019-05-10 18:00:16 +0900

    Update Client.js

--- src/Apollo/Client.js
+++ src/Apollo/Client.js
@@ -2,7 +2,8 @@ import ApolloClient from 'apollo-boost';
 import { defaults, resolvers } from "./LocaleState"
 
 export default new ApolloClient({
-     uri: "http://hellojw.net:9333",
+     uri: "https://herokuprisserver.herokuapp.com"
+     //uri: "http://hellojw.net:9333",
      //uri: "http://localhost:4000",
      clientState:{
           defaults,
